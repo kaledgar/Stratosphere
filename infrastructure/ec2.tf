83beba0f7979e30520a69a7c6cded2ed754e15b0
kkinastowski66 <kacperlucas672@gmail.com>
2024-01-21 21:37:09 +0100

    minor updates to gitignore

--- infrastructure/ec2.tf
+++ infrastructure/ec2.tf
@@ -15,3 +15,22 @@ module "ec2_instance" {
     environment = "dev"
   }
 }
+
+# RSA KEY PAIR
+
+resource "tls_private_key" "rsa_4096" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "deployer_key" {
+  key_name   = "deployer-key"
+  public_key = tls_private_key.rsa_4096.public_key_openssh
+}
+
+resource "local_file" "private_key" {
+  content  = tls_private_key.rsa_4096.private_key_pem
+  filename = "deployer-key"
+}
+
+
